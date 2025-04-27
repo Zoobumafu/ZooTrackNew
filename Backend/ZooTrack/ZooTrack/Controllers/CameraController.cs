558099776ee3b90ee9c6131df07d49010520800d
ShlomoVovek <110291682+ShlomoVovek@users.noreply.github.com>
2025-04-27 18:43:57 +0300

    delete forecast, change dir of "cameraController"

--- Backend/ZooTrack/ZooTrack/Controllers/CameraController.cs
+++ Backend/ZooTrack/ZooTrack/Controllers/CameraController.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ZooTrack.Services;
 
-namespace ZooTrack.Controllers
+namespace ZooTrackBackend.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
